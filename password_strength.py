95d5ecae65e589e76c1e3535e5090dd5f6ec8fae
Navneeth Rajeev <navneeth3009@gmail.com>
2026-01-09 11:01:26 +0300

    removed comments

--- password_strength.py
+++ password_strength.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-#TODO: Gen Password
 
 def generate_password(length, charset):
     return ''.join(random.choice(charset) for _ in range(length))
@@ -18,21 +17,21 @@ labels = []
 for _ in range(200):
     pwd = generate_password(random.randint(4, 6), string.ascii_lowercase)
     passwords.append(pwd)
-    labels.append(0)  # Weak
+    labels.append(0)
 
 # Medium passwords
 for _ in range(200):
     charset = string.ascii_lowercase + string.digits
     pwd = generate_password(random.randint(7, 9), charset)
     passwords.append(pwd)
-    labels.append(1)  # Medium
+    labels.append(1)
 
 # Strong passwords
 for _ in range(200):
     charset = string.ascii_letters + string.digits + string.punctuation
     pwd = generate_password(random.randint(10, 14), charset)
     passwords.append(pwd)
-    labels.append(2)  # Strong
+    labels.append(2)
 
 
 def extract_features(password):
@@ -46,19 +45,16 @@ def extract_features(password):
 X = np.array([extract_features(p) for p in passwords])
 y = np.array(labels)
 
-#TODO: Train-test split
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=42, stratify=y
 )
 
-#TODO: Model training
 
 model = SVC(kernel="rbf", C=1.0, gamma="scale")
 model.fit(X_train, y_train)
 
-#TODO: Evaluation
-
+# Prediction
 y_pred = model.predict(X_test)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
